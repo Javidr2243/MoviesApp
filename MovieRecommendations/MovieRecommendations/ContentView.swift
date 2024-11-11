2d14e4b717b3ff3eecb75626161d712119c2db41
Javier <javi_dr2@hotmail.com>
2024-11-11 12:10:33 +0100

    Project Creation

---
+++ MovieRecommendations/MovieRecommendations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieRecommendations
+//
+//  Created by JAVIER DAVILA RUIZ on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieRecommendations/MovieRecommendations/MovieRecommendationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecommendationsApp.swift
+//  MovieRecommendations
+//
+//  Created by JAVIER DAVILA RUIZ on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecommendationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
