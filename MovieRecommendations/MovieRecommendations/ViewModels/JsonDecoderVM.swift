2e093d90ea049e1a46ca80f2d1202f4e58c12083
Javier <javi_dr2@hotmail.com>
2024-11-13 10:33:09 +0100

    hello

---
+++ MovieRecommendations/MovieRecommendations/ViewModels/JsonDecoderVM.swift
@@ -0,0 +1,35 @@
+//
+//  JsonDecoderVM.swift
+//  MovieRecommendations
+//
+//  Created by JAVIER DAVILA RUIZ on 13/11/24.
+//
+
+import Foundation
+
+var movieData: [MovieElement] = load("csvjson-2.json")
+
+var userSimData: [UserSimulationElement] = load("UserSimulation.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    // Ensure the filename includes the correct extension
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
