de89bcba65dd8a20e10bdd78332970d5b3aa6ada
Jimena Gallegos <jimenagallegosr@outlook.com>
2024-11-13 23:29:17 +0100

    javi

---
+++ MovieRecommendations/MovieRecommendations/Views/RecMainView.swift
@@ -0,0 +1,18 @@
+//
+//  RecMainView.swift
+//  MovieRecommendations
+//
+//  Created by Jimena Gallegos on 13/11/24.
+//
+
+import SwiftUI
+
+struct RecMainView: View {
+    var body: some View {
+        ListofGroups()
+    }
+}
+
+#Preview {
+    RecMainView()
+}
