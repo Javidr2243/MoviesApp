793e6f9c73627ae0915caab2775e76991a40759e
Javier <javi_dr2@hotmail.com>
2024-11-15 09:00:50 +0100

    TabBar now is on darkmode

--- MovieRecommendations/MovieRecommendations/ContentView.swift
+++ MovieRecommendations/MovieRecommendations/ContentView.swift
@@ -8,24 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        customizeTabBarAppearance()
+    }
+    
     var body: some View {
         TabView {
             NavigationView { MovieAndGroupsView() }
-            .tabItem { Label("Home", systemImage: "film")}
-            .tag(0)
+                .tabItem {
+                    Label("Home", systemImage: "film")
+                }
+                .tag(0)
             
             NavigationView { MovieSearchView() }
-            .tabItem { Label("Search", systemImage: "magnifyingglass")}
-            .tag(1)
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+                .tag(1)
             
             NavigationView { RecMainView() }
-                .tabItem{Label("Groups", systemImage: "person.3.fill")}
+                .tabItem {
+                    Label("Groups", systemImage: "person.3.fill")
+                }
                 .tag(2)
-                
         }
+    }
+    
+    private func customizeTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.black
         
+        // Set item appearance
+        appearance.stackedLayoutAppearance.normal.iconColor = UIColor.lightGray
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
+        
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor.white
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 }
+
 #Preview {
     ContentView()
 }
