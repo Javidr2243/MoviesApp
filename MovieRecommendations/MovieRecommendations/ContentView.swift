7c77c741f5d022e9722bf0536c7e26fb535d11aa
Danna Karina González Valencia <a00833606@tec.mx>
2024-11-19 10:39:02 +0100

    TabBarColor

--- MovieRecommendations/MovieRecommendations/ContentView.swift
+++ MovieRecommendations/MovieRecommendations/ContentView.swift
@@ -10,6 +10,26 @@ import SwiftUI
 import SwiftUI
 
 struct ContentView: View {
+
+    
+    struct TabBarAppearanceModifier: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .onAppear {
+                    let appearance = UITabBarAppearance()
+                    appearance.configureWithOpaqueBackground()
+                    appearance.backgroundColor = UIColor.purpleApp
+                    appearance.stackedLayoutAppearance.selected.iconColor = UIColor.white
+                    appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
+                    appearance.stackedLayoutAppearance.normal.iconColor = UIColor.white.withAlphaComponent(0.6)
+                    appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white.withAlphaComponent(0.6)]
+                    
+                    UITabBar.appearance().standardAppearance = appearance
+                    UITabBar.appearance().scrollEdgeAppearance = appearance
+                }
+        }
+    }
+    
     var body: some View {
         TabView {
             NavigationView {
@@ -36,8 +56,9 @@ struct ContentView: View {
             }
             .tag(2)
         }
-        .preferredColorScheme(.dark)
+        .preferredColorScheme(.dark)      .modifier(TabBarAppearanceModifier())
     }
+
 }
 
 
